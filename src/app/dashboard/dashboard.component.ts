7a8aed5814d8719b1454f8970f37d9ba8354d528
Bishal Chettri <bishalin01@gmail.com>
2024-05-06 17:57:33 +0530

    dashboard component created

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,108 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api/menuitem';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css',
+})
+export class DashboardComponent implements OnInit {
+  years: number[];
+  quarters: string[];
+  priority: string;
+
+  stages: string[];
+  selectedStage: string;
+
+  categories: string[];
+  selectedCategory: string;
+
+  data: any;
+  ngOnInit() {
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--text-color');
+    this.data = {
+      labels: ['A', 'B', 'C'],
+      datasets: [
+        {
+          data: [300, 50, 100],
+          backgroundColor: [
+            documentStyle.getPropertyValue('--blue-500'),
+            documentStyle.getPropertyValue('--yellow-500'),
+            documentStyle.getPropertyValue('--green-500'),
+          ],
+          hoverBackgroundColor: [
+            documentStyle.getPropertyValue('--blue-400'),
+            documentStyle.getPropertyValue('--yellow-400'),
+            documentStyle.getPropertyValue('--green-400'),
+          ],
+        },
+      ],
+    };
+
+    this.years = [2024, 2023, 2022];
+    this.quarters = ['Q1', 'Q2', 'Q3'];
+
+    this.stages = ['raised', 'in_progress', 'review', 'closed'];
+    this.categories = ['A', 'B', 'C', 'D'];
+  }
+
+  // replace with actual stats from api
+  year: number = 2024;
+  quarter: string = 'Q1';
+
+  stats = [{ raised: 20, in_prog: 30, review: 30, closed: 30, resolved: 40}];
+
+  options: any = {
+    cutout: '60%',
+    plugins: {
+      legend: {
+        labels: {
+          color: '#fff',
+        },
+      },
+    },
+  };
+
+  // replace with actual tickets from api
+
+  tickets = [
+    {
+      ticket_id: 'asdfasd',
+      reporter: 'bishal',
+      created_at: '6/5/24',
+      category: 'Food and Beverages',
+      priority: 'Critical',
+      stage: 'raised',
+      assignee: 'admin',
+    },
+    {
+      ticket_id: 'vadjladw',
+      reporter: 'milind',
+      created_at: '5/5/24',
+      category: 'Maintenance',
+      priority: 'Important',
+      stage: 'raised',
+      assignee: 'admin',
+    },
+
+    {
+      ticket_id: 'llkjasdk',
+      reporter: 'aayushi',
+      created_at: '3/5/24',
+      category: 'seat management',
+      priority: 'Routine',
+      stage: 'raised',
+      assignee: 'admin',
+    },
+    {
+      ticket_id: 'qajlas1',
+      reporter: 'chetna',
+      created_at: '1/5/24',
+      category: 'id card',
+      priority: 'Critical',
+      stage: 'raised',
+      assignee: 'admin',
+    },
+  ];
+}
---
+++ src/app/header/sub-header/sub-header.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from "@angular/core";
+import { MenuItem } from "primeng/api/menuitem";
+
+
+@Component({
+    selector: 'app-sub-header',
+    templateUrl: './sub-header.component.html',
+    styleUrl: './sub-header.component.css'
+})
+export class SubHeaderComponent implements OnInit{
+    items: MenuItem[] | undefined;
+    ngOnInit() {
+        this.items = [
+            {
+              label: "Dashboard"
+            },
+            {
+              label: "Categories"
+            }
+        ];
+    }
+}
